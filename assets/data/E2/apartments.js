bed9886239a2e4f30783ed3ce7fe8a03cd5fead4
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2025-04-11 14:44:48 +0500

    E2,3,4 single apartment images added

--- assets/data/E2/apartments.js
+++ assets/data/E2/apartments.js
@@ -1,10 +1,52 @@
+import img49 from '~/assets/images/apartment/E2/49.webp';
+import img50 from '~/assets/images/apartment/E2/50.webp';
+import img51 from '~/assets/images/apartment/E2/51.webp';
+import img52 from '~/assets/images/apartment/E2/52.webp';
+import img53 from '~/assets/images/apartment/E2/53.webp';
+import img54 from '~/assets/images/apartment/E2/54.webp';
+import img55 from '~/assets/images/apartment/E2/55.webp';
+import img56 from '~/assets/images/apartment/E2/56.webp';
+import img57 from '~/assets/images/apartment/E2/57.webp';
+import img58 from '~/assets/images/apartment/E2/58.webp';
+import img59 from '~/assets/images/apartment/E2/59.webp';
+import img60 from '~/assets/images/apartment/E2/60.webp';
+import img61 from '~/assets/images/apartment/E2/61.webp';
+import img62 from '~/assets/images/apartment/E2/62.webp';
+import img63 from '~/assets/images/apartment/E2/63.webp';
+import img64 from '~/assets/images/apartment/E2/64.webp';
+import img65 from '~/assets/images/apartment/E2/65.webp';
+import img66 from '~/assets/images/apartment/E2/66.webp';
+import img67 from '~/assets/images/apartment/E2/67.webp';
+import img68 from '~/assets/images/apartment/E2/68.webp';
+import img69 from '~/assets/images/apartment/E2/69.webp';
+import img70 from '~/assets/images/apartment/E2/70.webp';
+import img71 from '~/assets/images/apartment/E2/71.webp';
+import img72 from '~/assets/images/apartment/E2/72.webp';
+import img73 from '~/assets/images/apartment/E2/73.webp';
+import img74 from '~/assets/images/apartment/E2/74.webp';
+import img75 from '~/assets/images/apartment/E2/75.webp';
+import img76 from '~/assets/images/apartment/E2/76.webp';
+import img77 from '~/assets/images/apartment/E2/77.webp';
+import img78 from '~/assets/images/apartment/E2/78.webp';
+import img79 from '~/assets/images/apartment/E2/79.webp';
+import img80 from '~/assets/images/apartment/E2/80.webp';
+import img81 from '~/assets/images/apartment/E2/81.webp';
+import img82 from '~/assets/images/apartment/E2/82.webp';
+import img83 from '~/assets/images/apartment/E2/83.webp';
+import img84 from '~/assets/images/apartment/E2/84.webp';
+import img85 from '~/assets/images/apartment/E2/85.webp';
+import img86 from '~/assets/images/apartment/E2/86.webp';
+import img87 from '~/assets/images/apartment/E2/87.webp';
+import img88 from '~/assets/images/apartment/E2/88.webp';
+import img89 from '~/assets/images/apartment/E2/89.webp';
+
 export const E2apartments = [
 	{
 		phaseId: '1',
 		apartmentId: '49',
 		blockId: 'E2',
 		floorId: '1',
-		img: 'IMG_HERE',
+		img: img49,
 		details: [
 			{
 				key_ru: 'Кухня + Зал',
@@ -49,7 +91,7 @@ export const E2apartments = [
 		apartmentId: '50',
 		blockId: 'E2',
 		floorId: '1',
-		img: 'IMG_HERE',
+		img: img50,
 		details: [
 			{
 				key_ru: 'Холл',
@@ -94,7 +136,7 @@ export const E2apartments = [
 		apartmentId: '51',
 		blockId: 'E2',
 		floorId: '1',
-		img: 'IMG_HERE',
+		img: img51,
 		details: [
 			{
 				key_ru: 'Холл',
