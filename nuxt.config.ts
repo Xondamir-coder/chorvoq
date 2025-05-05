a1676b128b81fcdb4d9a3b8928099cab5d3a6407
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2025-05-05 23:55:49 +0500

    modified nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,20 +12,29 @@ export default defineNuxtConfig({
 			}
 		}
 	},
-
 	app: {
 		head: {
 			titleTemplate: '%s | Chorvoq Darvozasi',
 			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }]
 		}
 	},
+	site: {
+		url: 'https://chorvoqdarvozasi.uz',
+		name: 'Chorvoq Darvozasi',
+		description:
+			'Find and compare apartments effortlessly—browse curated listings, explore high-resolution photos and floor plans, view detailed amenities, and download professional PDF brochures to make your next move with confidence.'
+	},
 	css: [
 		'~/assets/styles/main.scss' // Replace with the path to your SCSS file
 	],
-
 	i18n: {
+		locales: [
+			{ code: 'en', iso: 'en-US', name: 'English' },
+			{ code: 'ru', iso: 'ru-RU', name: 'Русский' },
+			{ code: 'uz', iso: 'uz-UZ', name: "O'zbek" }
+		],
+		defaultLocale: 'en',
 		vueI18n: '~/locales/i18n.config.js'
 	},
-
 	modules: ['@nuxtjs/i18n', '@pinia/nuxt', '@nuxtjs/seo']
 });
